7e12de4d23a5d1d2b7ac345f8c3032072dd34ae2
Lucas Alves <lucasalvesbs6@gmail.com>
2022-03-23 14:12:31 -0300

    style(helpers): rename functions

--- src/helpers/function.helper.ts
+++ src/helpers/function.helper.ts
@@ -2,13 +2,13 @@ import { BadRequestException, ForbiddenException } from '@nestjs/common';
 import { UsersEntity } from 'src/app/users/users.entity';
 import { MessageHelper } from './message.helper';
 
-export const verifyDuplicate = (role: UsersEntity) => {
+export const checkDuplicate = (role: UsersEntity) => {
   if (role) {
     throw new BadRequestException(MessageHelper.INFORMATIONS_INVALID);
   }
 };
 
-export const verifyTokenId = (req: any, user: UsersEntity) => {
+export const checkTokenId = (req: any, user: UsersEntity) => {
   if (req.id !== user.id) {
     throw new ForbiddenException(MessageHelper.FORBIDDEN);
   }
