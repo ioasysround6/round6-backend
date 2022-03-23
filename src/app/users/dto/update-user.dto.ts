902d55f054df0b6a75f2411420e58a09408a9af9
Lucas Alves <lucasalvesbs6@gmail.com>
2022-03-23 14:11:52 -0300

    feat(dto): add partialType

--- src/app/users/dto/update-user.dto.ts
+++ src/app/users/dto/update-user.dto.ts
@@ -1,24 +1,4 @@
-import { IsEmail, IsOptional, Matches } from 'class-validator';
-import { MessageHelper } from 'src/helpers/message.helper';
-import { RegExHelper } from 'src/helpers/regex.helper';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto {
-  @IsOptional()
-  @Matches(RegExHelper.name, { message: MessageHelper.FULL_NAME_VALID })
-  fullName: string;
-
-  @IsOptional()
-  @IsEmail()
-  email: string;
-
-  @IsOptional()
-  @Matches(RegExHelper.cpf, { message: MessageHelper.CPF_VALID })
-  cpf: string;
-
-  @IsOptional()
-  @Matches(RegExHelper.telephone, { message: MessageHelper.TELEPHONE_VALID })
-  telephone: string;
-
-  @IsOptional()
-  gender: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
