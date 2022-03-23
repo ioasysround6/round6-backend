b17867c6326425783fc3063dbbb260b2859d5b44
Lucas Alves <lucasalvesbs6@gmail.com>
2022-03-22 21:21:35 -0300

    style: add comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,15 @@ import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UsersModule } from './app/users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
-import { WinstonModule } from 'nest-winston';
-import { winstonTransport } from './config/transports/winston.transport';
-import { LoggerInterceptor } from './config/interceptors/logger.interceptor';
+import { APP_GUARD /* APP_INTERCEPTOR */ } from '@nestjs/core';
+//import { WinstonModule } from 'nest-winston';
+//import { winstonTransport } from './config/transports/winston.transport';
+//import { LoggerInterceptor } from './config/interceptors/logger.interceptor';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    WinstonModule.forRoot(winstonTransport),
+    //WinstonModule.forRoot(winstonTransport),
     TypeOrmModule.forRoot({
       type: process.env.DB_CONNECTION,
       url: process.env.DATABASE_URL,
@@ -33,10 +33,10 @@ import { LoggerInterceptor } from './config/interceptors/logger.interceptor';
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
     },
-    {
+    /* {
       provide: APP_INTERCEPTOR,
       useClass: LoggerInterceptor,
-    },
+    }, */
   ],
 })
 export class AppModule {}
