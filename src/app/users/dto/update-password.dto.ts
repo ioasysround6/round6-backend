9fd58fbe689f9a7102b2fddc3b2d3249950f949e
Lucas Alves <lucasalvesbs6@gmail.com>
2022-03-23 14:10:18 -0300

    style(users): rename UpdateUserPasswordDto to UpdatePasswordDto class

--- src/app/users/dto/update-password.dto.ts
+++ src/app/users/dto/update-password.dto.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty, Matches, MaxLength, MinLength } from 'class-validator';
 import { MessageHelper } from 'src/helpers/message.helper';
 import { RegExHelper } from 'src/helpers/regex.helper';
 
-export class UpdateUserPasswordDto {
+export class UpdatePasswordDto {
   @IsNotEmpty()
   @MinLength(8, { message: MessageHelper.PASSWORD_MIN_VALID })
   @MaxLength(30, { message: MessageHelper.PASSWORD_MAX_VALID })
