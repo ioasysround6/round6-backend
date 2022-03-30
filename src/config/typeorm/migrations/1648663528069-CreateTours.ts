3981621af80d9c71aa23f73dd544000762e79a74
Lucas Alves <lucasalvesbs6@gmail.com>
2022-03-30 15:09:06 -0300

    feat(typeorm): add tours migration

---
+++ src/config/typeorm/migrations/1648663528069-CreateTours.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTours1648663528069 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        CREATE TABLE tours (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name varchar(255) NOT NULL,
+        description text NOT NULL,
+        price decima(10, 2) NOT NULL,
+        photo varchar,
+        created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+        updated_at timestamp DEFAULT CURRENT_TIMESTAMP,
+        deleted_at timestamp 
+    );`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DROP TABLE tours');
+  }
+}
