e81417eafa5133b4c944c64c16bfa43be19233e1
Lucas Alves <lucasalvesbs6@gmail.com>
2022-03-31 13:02:17 -0300

    feat(crypto): add admin password hash

--- src/config/typeorm/migrations/1648686633528-InsertAdmin.ts
+++ src/config/typeorm/migrations/1648686633528-InsertAdmin.ts
@@ -1,20 +1,17 @@
-import { hashSync } from 'bcrypt';
 import { Role } from '../../enum/role.enum';
 import { MigrationInterface, QueryRunner } from 'typeorm';
-
-const adminPassword = process.env.ADMIN_PASSWORD;
-const hashedPassword = hashSync(adminPassword, 10);
+import { adminPassword } from '../../../helpers/crypto.helper';
 
 export class InsertAdmin1648686633528 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `INSERT INTO users(id, first_name, last_name, email, password, birth_date, role, created_at, updated_at) VALUES(uuid_generate_v4(), '${process.env.ADMIN_FIRST_NAME}', '${process.env.ADMIN_LAST_NAME}', '${process.env.ADMIN_EMAIL}', '${hashedPassword}', '${process.env.ADMIN_BIRTH_DATE}', '${Role.Admin}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)`,
+      `INSERT INTO users(id, first_name, last_name, email, password, birth_date, role, created_at, updated_at) VALUES(uuid_generate_v4(), '${process.env.ADMIN_FIRST_NAME}', '${process.env.ADMIN_LAST_NAME}', '${process.env.ADMIN_EMAIL}', '${adminPassword}', '${process.env.ADMIN_BIRTH_DATE}', '${Role.Admin}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `DELETE FROM users WHERE id, first_name, last_name, email, password, birth_date, role, created_at, updated_at = uuid_generate_v4(), '${process.env.ADMIN_FIRST_NAME}', '${process.env.ADMIN_LAST_NAME}', '${process.env.ADMIN_EMAIL}', '${hashedPassword}', '${process.env.ADMIN_BIRTH_DATE}', '${Role.Admin}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP`,
+      `DELETE FROM users WHERE id, first_name, last_name, email, password, birth_date, role, created_at, updated_at = uuid_generate_v4(), '${process.env.ADMIN_FIRST_NAME}', '${process.env.ADMIN_LAST_NAME}', '${process.env.ADMIN_EMAIL}', '${adminPassword}', '${process.env.ADMIN_BIRTH_DATE}', '${Role.Admin}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP`,
     );
   }
 }
--- src/helpers/crypto.helper.ts
+++ src/helpers/crypto.helper.ts
@@ -1,3 +1,4 @@
+import { hashSync } from 'bcrypt';
 import { EncryptionTransformer } from 'typeorm-encrypted';
 require('dotenv/config');
 
@@ -7,3 +8,5 @@ export const dataEncryption = new EncryptionTransformer({
   ivLength: parseInt(process.env.AES_IV_LENGTH),
   iv: process.env.AES_IV,
 });
+
+export const adminPassword = hashSync(process.env.ADMIN_PASSWORD, 10);
