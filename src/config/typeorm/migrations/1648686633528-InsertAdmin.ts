8791b49d899612a009eb335a6d25c3b823f15cce
Lucas Alves <lucasalvesbs6@gmail.com>
2022-03-31 14:53:24 -0300

    refactor(crypto): add environment variable in heroku

--- src/config/typeorm/migrations/1648686633528-InsertAdmin.ts
+++ src/config/typeorm/migrations/1648686633528-InsertAdmin.ts
@@ -1,16 +1,19 @@
+import { hashSync } from 'bcrypt';
 import { Role } from '../../enum/role.enum';
 import { MigrationInterface, QueryRunner } from 'typeorm';
+
+const hashedPassword = hashSync(process.env.ADMIN_PASSWORD, 10);
 
 export class InsertAdmin1648686633528 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `INSERT INTO users(id, first_name, last_name, email, password, birth_date, role, created_at, updated_at) VALUES(uuid_generate_v4(), '${process.env.ADMIN_FIRST_NAME}', '${process.env.ADMIN_LAST_NAME}', '${process.env.ADMIN_EMAIL}', '${process.env.ADMIN_PASSWORD}', '${process.env.ADMIN_BIRTH_DATE}', '${Role.Admin}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)`,
+      `INSERT INTO users(id, first_name, last_name, email, password, birth_date, role, created_at, updated_at) VALUES(uuid_generate_v4(), '${process.env.ADMIN_FIRST_NAME}', '${process.env.ADMIN_LAST_NAME}', '${process.env.ADMIN_EMAIL}', '${hashedPassword}', '${process.env.ADMIN_BIRTH_DATE}', '${Role.Admin}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `DELETE FROM users WHERE id, first_name, last_name, email, password, birth_date, role, created_at, updated_at = uuid_generate_v4(), '${process.env.ADMIN_FIRST_NAME}', '${process.env.ADMIN_LAST_NAME}', '${process.env.ADMIN_EMAIL}', '${process.env.ADMIN_PASSWORD}', '${process.env.ADMIN_BIRTH_DATE}', '${Role.Admin}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP`,
+      `DELETE FROM users WHERE id, first_name, last_name, email, password, birth_date, role, created_at, updated_at = uuid_generate_v4(), '${process.env.ADMIN_FIRST_NAME}', '${process.env.ADMIN_LAST_NAME}', '${process.env.ADMIN_EMAIL}', '${hashedPassword}', '${process.env.ADMIN_BIRTH_DATE}', '${Role.Admin}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP`,
     );
   }
 }
