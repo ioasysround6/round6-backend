6036deb5432277288f4a89790e82a65bedb18d2e
Lucas Alves <lucasalvesbs6@gmail.com>
2022-04-01 13:44:14 -0300

    refactor(helpers): remove telephone and zipCode regex

--- src/helpers/regex.helper.ts
+++ src/helpers/regex.helper.ts
@@ -5,14 +5,8 @@ const name = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚ
 
 const cpf = /^[0-9]{11}/;
 
-const telephone = /^[1-9]{2}(?:[2-8]|9[1-9])[0-9]{7}$/;
-
-const zipCode = /^[0-9]{8}$/;
-
 export const RegExHelper = {
   password,
   name,
   cpf,
-  telephone,
-  zipCode,
 };
