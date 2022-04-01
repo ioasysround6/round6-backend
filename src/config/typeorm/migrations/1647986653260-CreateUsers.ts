840760773031c653f4043e9e11eecc67f3b94734
Lucas Alves <lucasalvesbs6@gmail.com>
2022-03-31 23:23:22 -0300

    style(migrations): remove white space

--- src/config/typeorm/migrations/1647986653260-CreateUsers.ts
+++ src/config/typeorm/migrations/1647986653260-CreateUsers.ts
@@ -4,7 +4,7 @@ export class CreateUsers1647986653260 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       CREATE TYPE role_options AS ENUM ('tourist', 'admin');
-        CREATE TABLE users (
+      CREATE TABLE users (
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         first_name varchar(255) NOT NULL,
         last_name varchar(255) NOT NULL,
