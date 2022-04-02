87bc9f19ad53269b9ecb9b31ad32de93d6080b6a
Lucas Alves <lucasalvesbs6@gmail.com>
2022-04-02 12:59:42 -0300

    feat(dto): do not allow the user to place the order with quantity 0

--- src/app/orders/dto/create-order.dto.ts
+++ src/app/orders/dto/create-order.dto.ts
@@ -1,10 +1,11 @@
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsNumber, Min } from 'class-validator';
 import { ToursEntity } from 'src/app/tours/tours.entity';
 import { UsersEntity } from 'src/app/users/users.entity';
 
 export class CreateOrderDto {
   @IsNotEmpty()
   @IsNumber()
+  @Min(1)
   amountPeople: number;
 
   @IsNotEmpty()
