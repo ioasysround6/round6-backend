314bbf55c6997aa79144a40be5d42a6849a9b4a7
Lucas Alves <lucasalvesbs6@gmail.com>
2022-04-02 13:05:34 -0300

    feat(seeds): add admin and tourists seed with migrations

--- src/config/typeorm/migrations/1648842847683-InsertTouristOne.ts
+++ src/config/typeorm/migrations/1648842847683-InsertTouristOne.ts
@@ -2,19 +2,19 @@ import { hashSync } from 'bcrypt';
 import { Role } from '../../enum/role.enum';
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-const hashedPassword = hashSync(process.env.ADMIN_PASSWORD, 10);
+const hashedPassword = hashSync(process.env.TOURIST1_PASSWORD, 10);
 
-export class InsertAdmin1648686633528 implements MigrationInterface {
+export class InsertTouristOne1648842847683 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `INSERT INTO users(first_name, last_name, email, password, birth_date, role) VALUES($1, $2, $3, $4, $5, $6)`,
       [
-        process.env.ADMIN_FIRST_NAME,
-        process.env.ADMIN_LAST_NAME,
-        process.env.ADMIN_EMAIL,
+        process.env.TOURIST1_FIRST_NAME,
+        process.env.TOURIST1_LAST_NAME,
+        process.env.TOURIST1_EMAIL,
         hashedPassword,
-        process.env.ADMIN_BIRTH_DATE,
-        Role.Admin,
+        process.env.TOURIST1_BIRTH_DATE,
+        Role.Tourist,
       ],
     );
   }
@@ -23,12 +23,12 @@ export class InsertAdmin1648686633528 implements MigrationInterface {
     await queryRunner.query(
       `DELETE FROM users WHERE first_name, last_name, email, password, birth_date, role = $1 $2, $3, $4, $5, $6`,
       [
-        process.env.ADMIN_FIRST_NAME,
-        process.env.ADMIN_LAST_NAME,
-        process.env.ADMIN_EMAIL,
+        process.env.TOURIST1_FIRST_NAME,
+        process.env.TOURIST1_LAST_NAME,
+        process.env.TOURIST1_EMAIL,
         hashedPassword,
-        process.env.ADMIN_BIRTH_DATE,
-        Role.Admin,
+        process.env.TOURIST1_BIRTH_DATE,
+        Role.Tourist,
       ],
     );
   }
--- src/config/typeorm/migrations/1648842920690-InsertTouristTwo.ts
+++ src/config/typeorm/migrations/1648842920690-InsertTouristTwo.ts
@@ -2,19 +2,19 @@ import { hashSync } from 'bcrypt';
 import { Role } from '../../enum/role.enum';
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-const hashedPassword = hashSync(process.env.ADMIN_PASSWORD, 10);
+const hashedPassword = hashSync(process.env.TOURIST2_PASSWORD, 10);
 
-export class InsertAdmin1648686633528 implements MigrationInterface {
+export class InsertTouristTwo1648842920690 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `INSERT INTO users(first_name, last_name, email, password, birth_date, role) VALUES($1, $2, $3, $4, $5, $6)`,
       [
-        process.env.ADMIN_FIRST_NAME,
-        process.env.ADMIN_LAST_NAME,
-        process.env.ADMIN_EMAIL,
+        process.env.TOURIST2_FIRST_NAME,
+        process.env.TOURIST2_LAST_NAME,
+        process.env.TOURIST2_EMAIL,
         hashedPassword,
-        process.env.ADMIN_BIRTH_DATE,
-        Role.Admin,
+        process.env.TOURIST2_BIRTH_DATE,
+        Role.Tourist,
       ],
     );
   }
@@ -23,12 +23,12 @@ export class InsertAdmin1648686633528 implements MigrationInterface {
     await queryRunner.query(
       `DELETE FROM users WHERE first_name, last_name, email, password, birth_date, role = $1 $2, $3, $4, $5, $6`,
       [
-        process.env.ADMIN_FIRST_NAME,
-        process.env.ADMIN_LAST_NAME,
-        process.env.ADMIN_EMAIL,
+        process.env.TOURIST2_FIRST_NAME,
+        process.env.TOURIST2_LAST_NAME,
+        process.env.TOURIST2_EMAIL,
         hashedPassword,
-        process.env.ADMIN_BIRTH_DATE,
-        Role.Admin,
+        process.env.TOURIST2_BIRTH_DATE,
+        Role.Tourist,
       ],
     );
   }
