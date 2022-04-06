a78c1ad093c1ab78587ad97ba98311d8e595411c
Lucas Alves <lucasalvesbs6@gmail.com>
2022-04-06 18:29:08 -0300

    feat(dto): add IsString decorator in password property

--- src/app/users/dto/update-password.dto.ts
+++ src/app/users/dto/update-password.dto.ts
@@ -1,9 +1,16 @@
-import { IsNotEmpty, Matches, MaxLength, MinLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { MessageHelper } from 'src/helpers/message.helper';
 import { RegExHelper } from 'src/helpers/regex.helper';
 
 export class UpdatePasswordDto {
   @IsNotEmpty()
+  @IsString()
   @MinLength(8, { message: MessageHelper.PASSWORD_MIN_VALID })
   @MaxLength(30, { message: MessageHelper.PASSWORD_MAX_VALID })
   @Matches(RegExHelper.password, { message: MessageHelper.PASSWORD_VALID })
