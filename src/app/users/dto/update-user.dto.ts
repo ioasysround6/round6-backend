392d9aa82de6f80cd28e3e454c052d5a774fd30e
Lucas Alves <lucasalvesbs6@gmail.com>
2022-04-06 13:18:01 -0300

    refactor(dto): remove birthDate property on UpdateUserDto

--- src/app/users/dto/update-user.dto.ts
+++ src/app/users/dto/update-user.dto.ts
@@ -1,4 +1,38 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { MessageHelper } from 'src/helpers/message.helper';
+import { RegExHelper } from 'src/helpers/regex.helper';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @Matches(RegExHelper.name, { message: MessageHelper.FIRST_NAME_VALID })
+  firstName: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(RegExHelper.name, { message: MessageHelper.LAST_NAME_VALID })
+  lastName: string;
+
+  @IsOptional()
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(8, { message: MessageHelper.PASSWORD_MIN_VALID })
+  @MaxLength(30, { message: MessageHelper.PASSWORD_MAX_VALID })
+  @Matches(RegExHelper.password, { message: MessageHelper.PASSWORD_VALID })
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  photo: string;
+}
