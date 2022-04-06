f25c266cd55c22676e5ca35daaf68772fe8d7a4c
Lucas Alves <lucasalvesbs6@gmail.com>
2022-04-05 22:21:15 -0300

    refactor(helpers): remove date regex

--- src/helpers/regex.helper.ts
+++ src/helpers/regex.helper.ts
@@ -3,10 +3,7 @@ const password =
 
 const name = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ\-'/ ]+$/;
 
-const date = /(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/](19|20)\d{2}/;
-
 export const RegExHelper = {
   password,
   name,
-  date,
 };
