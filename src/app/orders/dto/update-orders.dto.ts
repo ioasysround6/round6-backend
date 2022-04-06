21f9a2d3a2cc9898400a6a3db67f6d8ca8d3d797
Lucas Alves <lucasalvesbs6@gmail.com>
2022-04-06 16:22:13 -0300

    refactor(dto): remove tour property from UpdateOrderDto

--- src/app/orders/dto/update-orders.dto.ts
+++ src/app/orders/dto/update-orders.dto.ts
@@ -1,4 +1,8 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateOrderDto } from './create-order.dto';
+import { IsNotEmpty, IsNumber, Min } from 'class-validator';
 
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
+export class UpdateOrderDto {
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
+  amountPeople: number;
+}
