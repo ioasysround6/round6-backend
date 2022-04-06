7189c861c915019a4308120410af9f8dd2676879
Lucas Alves <lucasalvesbs6@gmail.com>
2022-04-05 22:20:37 -0300

    feat(helpers): add checkDate function
    the objective is not to allow the registration of people under the age of 18

--- src/helpers/function.helper.ts
+++ src/helpers/function.helper.ts
@@ -1,15 +1,21 @@
-import { BadRequestException, ForbiddenException } from '@nestjs/common';
+import { BadRequestException, ConflictException } from '@nestjs/common';
 import { UsersEntity } from 'src/app/users/users.entity';
 import { MessageHelper } from './message.helper';
 
 export const checkDuplicate = (role: UsersEntity) => {
   if (role) {
-    throw new BadRequestException(MessageHelper.INFORMATIONS_INVALID);
+    throw new ConflictException(MessageHelper.CONFLICT);
   }
 };
 
-export const checkTokenId = (req: any, user: UsersEntity) => {
-  if (req.id !== user.id) {
-    throw new ForbiddenException(MessageHelper.FORBIDDEN);
+export const checkDate = (date: Date) => {
+  const birthDate = new Date(date);
+  const currentDate = new Date();
+  const differenceTime = Math.abs(currentDate.getTime() - birthDate.getTime());
+  const differenceDays = Math.ceil(differenceTime / (1000 * 60 * 60 * 24));
+  const yearsOld = Math.floor(differenceDays / 365.23);
+  console.log(yearsOld);
+  if (yearsOld < 18) {
+    throw new BadRequestException(MessageHelper.DATE_VALID);
   }
 };
