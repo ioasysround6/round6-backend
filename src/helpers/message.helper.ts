1ba7096a269c657bd71ecbededfb1f9b2ceed2af
Lucas Alves <lucasalvesbs6@gmail.com>
2022-04-05 22:31:03 -0300

    refactor(helpers): remove INTERNAL_SERVER_ERROR and INFORMATIONS_INVALID messages

--- src/helpers/message.helper.ts
+++ src/helpers/message.helper.ts
@@ -4,13 +4,12 @@ export const MessageHelper = {
   FIRST_NAME_VALID: 'Nome deve conter apenas letras, hífen e/ou aspas simples',
   LAST_NAME_VALID:
     'Sobrenome deve conter apenas letras, hífen e/ou aspas simples',
-  PASSWORD_OR_EMAIL_INVALID: 'Credenciais inválidas',
+  PASSWORD_OR_EMAIL_INVALID: 'Usuário ou senha inválidos',
   PASSWORD_MIN_VALID: 'A senha deve conter pelo menos 8 caracteres',
   PASSWORD_MAX_VALID: 'A senha deve ter no máximo 30 caracteres',
-  DATE_VALID: 'A data deve estar no formato XX/XX/XXXX',
-  INFORMATIONS_INVALID: 'Informações inválidas',
+  DATE_VALID: 'Para se cadastrar, é necessário ter no mínimo 18 anos',
   BAD_REQUEST: 'Alguma informação está errada',
   NOT_FOUND: 'Infelizmente, a página buscada não foi encontrada',
   FORBIDDEN: 'Privilégios insuficientes para acessar a página',
-  INTERNAL_SERVER_ERROR: 'Opa! Algo errado aconteceu',
+  CONFLICT: 'Esse email já existe',
 };
