8736cc562cad0faa83adce5e18ef815b78539ad0
Lucas Alves <lucasalvesbs6@gmail.com>
2022-04-15 12:34:30 -0300

    feat(helpers): add cardNumber, printedName, securityCode and cpf regex

--- src/helpers/regex.helper.ts
+++ src/helpers/regex.helper.ts
@@ -1,9 +1,18 @@
 const password =
   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
 
-const name = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ\-'/ ]+$/;
+const cardNumber = /^[0-9]{4} [0-9]{4} [0-9]{4} [0-9]{4}$/;
+
+const printedName = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ\-'. ]+$/;
+
+const securityCode = /^[0-9]{3}/;
+
+const cpf = /^([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})$/;
 
 export const RegExHelper = {
   password,
-  name,
+  cardNumber,
+  securityCode,
+  printedName,
+  cpf,
 };
