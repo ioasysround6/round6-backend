bba7f4acb9021f6b1a4f489129dd50ef1ce59df5
Lucas Alves <lucasalvesbs6@gmail.com>
2022-04-15 12:37:10 -0300

    feat(helpers): add TOKEN_INVALID  message

--- src/helpers/message.helper.ts
+++ src/helpers/message.helper.ts
@@ -1,19 +1,27 @@
 export const MessageHelper = {
   PASSWORD_VALID:
     'A senha deve conter letras maiúsculas, minúsculas, números e caracteres especiais',
-  FIRST_NAME_VALID: 'Nome deve conter apenas letras, hífen e/ou aspas simples',
-  LAST_NAME_VALID:
-    'Sobrenome deve conter apenas letras, hífen e/ou aspas simples',
   PASSWORD_OR_EMAIL_INVALID: 'Usuário ou senha inválidos',
   PASSWORD_MIN_VALID: 'A senha deve conter pelo menos 8 caracteres',
   PASSWORD_MAX_VALID: 'A senha deve ter no máximo 30 caracteres',
   DATE_VALID: 'Para se cadastrar, é necessário ter no mínimo 18 anos',
+  METHOD_VALID: 'Método de pagamento deve ser cash, credit_card ou pix',
+  CARD_NUMBER_VALID:
+    'Número de cartão de crédito deve ser no formato: 0000 0000 0000 0000',
+  PRINTED_NAME_VALID:
+    'Nome impresso deve ser apenas letras, aspas simples, hífen e/ou ponto',
+  SECURITY_CODE_VALID: 'Código de segurança deve ser no formato: 000',
+  CPF_VALID: 'O cpf deve conter 11 números',
   BAD_REQUEST: 'Alguma informação está errada',
   NOT_FOUND: 'Infelizmente, a página buscada não foi encontrada',
   UNIDENTIFIED_TOUR: 'Passeio turístico não identificado',
+  UNIDENTIFIED_ORDER: 'Pedido de compra não identificado',
   FORBIDDEN: 'Privilégios insuficientes para acessar a página',
   CONFLICT: 'Esse email já existe',
   UNAVAILABLE_VACANCIES: 'Não é possível atender o número de vagas solicitadas',
   IMPOSSIBLE_UPDATE:
     'Não é possível atualizar o pedido para a mesma quantidade de pessoas que já contém no pacote',
+  CREDIT_CARD_INVALID:
+    'Cartão de crédito inválido! É necessário preencher todas as informações',
+  TOKEN_INVALID: 'Token inválido',
 };
