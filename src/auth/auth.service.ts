388f1de4cf25ffaaaeef5be74d273040c186f3d5
Lucas Alves <lucasalvesbs6@gmail.com>
2022-04-16 18:07:02 -0300

    feat(auth): add TokensService in login function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,17 +3,20 @@ import { UsersEntity } from 'src/app/users/users.entity';
 import { UsersService } from 'src/app/users/users.service';
 import { compareSync, hashSync } from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { TokensService } from 'src/app/tokens/tokens.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UsersService,
     private readonly jwtService: JwtService,
+    private readonly tokenService: TokensService,
   ) {}
 
   async login(user) {
     const payload = { sub: user.id, role: user.role };
     const token = this.jwtService.sign(payload);
+    await this.tokenService.saveToken(token, user.id);
     user.password = undefined;
     user.deletedAt = undefined;
     return {
