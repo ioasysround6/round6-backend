a1e4821c26dc3e444eb81bec2f64d1201c311f3a
Lucas Alves <lucasalvesbs6@gmail.com>
2022-04-20 11:51:19 -0300

    refactor(dto): remove order property in update

--- src/app/checkouts/dto/update-checkout.dto.ts
+++ src/app/checkouts/dto/update-checkout.dto.ts
@@ -1,4 +1,33 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCheckoutDto } from './create-checkout.dto';
+import {
+  IsDateString,
+  IsEmail,
+  IsOptional,
+  IsString,
+  Matches,
+} from 'class-validator';
+import { MessageHelper } from 'src/helpers/message.helper';
+import { RegExHelper } from 'src/helpers/regex.helper';
 
-export class UpdateCheckoutDto extends PartialType(CreateCheckoutDto) {}
+export class UpdateCheckoutDto {
+  @IsOptional()
+  @IsString()
+  firstName: string;
+
+  @IsOptional()
+  @IsString()
+  lastName: string;
+
+  @IsOptional()
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsDateString()
+  birthDate: Date;
+
+  @IsOptional()
+  @IsString()
+  @Matches(RegExHelper.cpf, { message: MessageHelper.CPF_VALID })
+  cpf: string;
+}
--- src/app/payments/dto/update-payment.dto.ts
+++ src/app/payments/dto/update-payment.dto.ts
@@ -1,4 +1,46 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePaymentDto } from './create-payment.dto';
+import {
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsString,
+  Matches,
+  Min,
+} from 'class-validator';
+import { Method } from 'src/config/enum/method.enum';
+import { MessageHelper } from 'src/helpers/message.helper';
+import { RegExHelper } from 'src/helpers/regex.helper';
 
-export class UpdatePaymentDto extends PartialType(CreatePaymentDto) {}
+export class UpdatePaymentDto {
+  @IsOptional()
+  @IsEnum(Method, { message: MessageHelper.METHOD_VALID })
+  method: Method;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  installments: number;
+
+  @IsOptional()
+  @IsString()
+  @Matches(RegExHelper.cardNumber, { message: MessageHelper.CARD_NUMBER_VALID })
+  cardNumber: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(RegExHelper.printedName, {
+    message: MessageHelper.PRINTED_NAME_VALID,
+  })
+  printedName: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(RegExHelper.dueDate, { message: MessageHelper.DUE_DATE_VALID })
+  dueDate: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(RegExHelper.securityCode, {
+    message: MessageHelper.SECURITY_CODE_VALID,
+  })
+  securityCode: string;
+}
